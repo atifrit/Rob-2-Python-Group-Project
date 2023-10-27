9f84ff48b96229f885482fdeb8671633fd89a55a
Jon <jondiezv@gmail.com>
2023-10-27 18:40:27 -0400

    Fixed the chart state refreshing on everything single action

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -28,91 +28,12 @@ const PortfolioDetails = () => {
   const [portfolioBalance, setPortfolioBalance] = useState(null);
   const [isCreateWatchlistDisabled, setIsCreateWatchlistDisabled] =
     useState(true);
+  const [chartData, setChartData] = useState(null);
+  const [reRendered, setReRendered] = useState(false);
 
-  useEffect(() => {
-    if (!currentUserPortfolio) {
-      dispatch(getUserPortfolio());
-    }
-    if (!currentUserWatchlist) {
-      dispatch(getUserWatchlist());
-    }
-
-    const handleClickOutsideModal = (e) => {
-      if (modalContent && isModalOpen && !e.target.closest(".modal-content")) {
-        setIsModalOpen(false);
-        setModalContent(null);
-      }
-    };
-
-    document.addEventListener("click", handleClickOutsideModal);
-
-    return () => {
-      document.removeEventListener("click", handleClickOutsideModal);
-    };
-  }, [
-    dispatch,
-    currentUserPortfolio,
-    currentUserWatchlist,
-    modalContent,
-    isModalOpen,
-    setModalContent,
-  ]);
-
-  useEffect(() => {
-    if (Object.values(allCompanies).length < 1) {
-      dispatch(getAllCompanies());
-    }
-
-    if (
-      currentUserPortfolio !== null &&
-      currentUserPortfolio.balance !== null
-    ) {
-      setIsLoading(false);
-      setPortfolioBalance(currentUserPortfolio.balance);
-    }
-  }, [dispatch, allCompanies, currentUserPortfolio]);
-
-  if (!currentUserPortfolio || !currentUserWatchlist) {
-    return <div>Loading...</div>;
-  }
-
-  const transactions = currentUserPortfolio.transactions || [];
-
-  const handleCreateWatchlist = () => {
-    if (newWatchlistName.trim() !== "") {
-      dispatch(createNewWatchlist(newWatchlistName));
-      setNewWatchlistName("");
-      setIsCreateWatchlistDisabled(true);
-    }
-  };
-  const handleDeleteWatchlist = (watchlistId) => {
-    setIsModalOpen(true);
-    setModalContent(
-      <DeleteWatchlistFormModal
-        watchlistId={watchlistId}
-        onClose={() => setIsModalOpen(false)}
-      />
-    );
-  };
-
-  const handleRemoveFunds = () => {
-    setIsModalOpen(true);
-    setModalContent(<RemoveFundsModal onClose={() => setIsModalOpen(false)} />);
-  };
-
-  let allPrices;
-
-  if (Object.values(allCompanies).length) {
-    allPrices = transactions.map((transaction, index) => {
-      let resindex;
-      for (let i = 0; i < Object.values(allCompanies).length; i++) {
-        if (transaction.ticker === allCompanies[i][i + 1].ticker) {
-          resindex = i;
-        }
-      }
-      return { [transaction.ticker]: allCompanies[resindex][resindex + 1] };
-    });
-  }
+  const transactions = currentUserPortfolio
+    ? currentUserPortfolio.transactions || []
+    : [];
 
   let options = {
     title: {
@@ -160,72 +81,181 @@ const PortfolioDetails = () => {
     responsive: true,
   };
 
-  function choose(arr) {
-    let i = Math.floor(Math.random() * arr.length);
-    return arr[i];
-  }
-
-  let Progressions = [
-    [1, 2, -1, 2, 3, -1],
-    [-1, -1, 1, -1, -1, 1],
-  ];
-
-  function priceGenerator(base, num, progressions) {
-    let trend = choose(progressions);
-
-    let prices = [];
-    let val = base;
-
-    for (let i = 0; i < num; i++) {
-      let stockval = val + choose(trend) * Math.random();
-      prices.push(stockval.toFixed(2));
-      val = stockval;
+  useEffect(() => {
+    if (!currentUserPortfolio) {
+      dispatch(getUserPortfolio());
+    }
+    if (!currentUserWatchlist) {
+      dispatch(getUserWatchlist());
     }
 
-    return prices;
-  }
+    const handleClickOutsideModal = (e) => {
+      if (modalContent && isModalOpen && !e.target.closest(".modal-content")) {
+        setIsModalOpen(false);
+        setModalContent(null);
+      }
+    };
 
-  let priceArrs = [];
+    document.addEventListener("click", handleClickOutsideModal);
 
-  if (allPrices && allPrices.length) {
-    for (let i = 0; i < allPrices.length; i++) {
-      const currentCompany = allPrices[i];
-      for (const ticker in currentCompany) {
-        if (currentCompany.hasOwnProperty(ticker)) {
-          const price = currentCompany[ticker].price;
-          priceArrs.push(priceGenerator(price, 30, Progressions));
+    return () => {
+      document.removeEventListener("click", handleClickOutsideModal);
+    };
+  }, [
+    dispatch,
+    currentUserPortfolio,
+    currentUserWatchlist,
+    modalContent,
+    isModalOpen,
+    setModalContent,
+  ]);
+
+  useEffect(() => {
+    if (Object.values(allCompanies).length < 1) {
+      dispatch(getAllCompanies());
+    }
+
+    if (
+      currentUserPortfolio !== null &&
+      currentUserPortfolio.balance !== null
+    ) {
+      setIsLoading(false);
+      setPortfolioBalance(currentUserPortfolio.balance);
+    }
+  }, [dispatch, allCompanies, currentUserPortfolio]);
+
+  useEffect(() => {
+    if (chartData === null) {
+      let allPrices;
+
+      if (Object.values(allCompanies).length) {
+        allPrices = transactions.map((transaction, index) => {
+          let resindex;
+          for (let i = 0; i < Object.values(allCompanies).length; i++) {
+            if (transaction.ticker === allCompanies[i][i + 1].ticker) {
+              resindex = i;
+            }
+          }
+          return { [transaction.ticker]: allCompanies[resindex][resindex + 1] };
+        });
+      }
+
+      function choose(arr) {
+        let i = Math.floor(Math.random() * arr.length);
+        return arr[i];
+      }
+
+      let Progressions = [
+        [1, 2, -1, 2, 3, -1],
+        [-1, -1, 1, -1, -1, 1],
+      ];
+
+      function priceGenerator(base, num, progressions) {
+        let trend = choose(progressions);
+
+        let prices = [];
+        let val = base;
+
+        for (let i = 0; i < num; i++) {
+          let stockval = val + choose(trend) * Math.random();
+          prices.push(stockval.toFixed(2));
+          val = stockval;
+        }
+
+        return prices;
+      }
+
+      let priceArrs = [];
+
+      if (allPrices && allPrices.length) {
+        for (let i = 0; i < allPrices.length; i++) {
+          const currentCompany = allPrices[i];
+          for (const ticker in currentCompany) {
+            if (currentCompany.hasOwnProperty(ticker)) {
+              const price = currentCompany[ticker].price;
+              priceArrs.push(priceGenerator(price, 30, Progressions));
+            }
+          }
         }
       }
-    }
-  }
 
-  let priceData = [];
-  if (priceArrs.length) {
-    for (let i = 0; i < priceArrs[0].length; i++) {
-      let sum = 0;
-      for (let j = 0; j < priceArrs.length; j++) {
-        const priceValue = parseFloat(priceArrs[j][i]);
-        sum += priceValue;
+      let priceData = [];
+      if (priceArrs.length) {
+        for (let i = 0; i < priceArrs[0].length; i++) {
+          let sum = 0;
+          for (let j = 0; j < priceArrs.length; j++) {
+            const priceValue = parseFloat(priceArrs[j][i]);
+            sum += priceValue;
+          }
+          priceData.push(sum);
+        }
       }
-      priceData.push(sum);
+
+      const newChartData = {
+        labels: [
+          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+          20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+        ].reverse(),
+        datasets: [
+          {
+            label: "Value",
+            data: priceData,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor:
+              priceData[0] < priceData[priceData.length - 1] ? "green" : "red",
+            borderWidth: 3,
+          },
+        ],
+      };
+      console.log("Updating chartData to:", newChartData);
+      console.log("Is Chart Data Null?", chartData === null);
+      console.log("Chart Data:", JSON.stringify(chartData));
+      setChartData(newChartData);
+
+      console.log("Updated Chart Data:", JSON.stringify(chartData));
     }
+  }, [chartData, allCompanies, transactions]);
+
+  useEffect(() => {
+    console.log("Updated Chart Data2:", JSON.stringify(chartData));
+  }, [chartData]);
+
+  useEffect(() => {
+    if (!reRendered) {
+      const timer = setTimeout(() => {
+        setChartData(null);
+        setReRendered(true);
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [reRendered]);
+
+  if (!currentUserPortfolio || !currentUserWatchlist) {
+    return <div>Loading...</div>;
   }
 
-  let data = {
-    labels: [
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-      21, 22, 23, 24, 25, 26, 27, 28, 29,
-    ].reverse(),
-    datasets: [
-      {
-        label: "Value",
-        data: priceData,
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
-        borderColor:
-          priceData[0] < priceData[priceData.length - 1] ? "green" : "red",
-        borderWidth: 3,
-      },
-    ],
+  const handleCreateWatchlist = () => {
+    if (newWatchlistName.trim() !== "") {
+      dispatch(createNewWatchlist(newWatchlistName));
+      setNewWatchlistName("");
+      setIsCreateWatchlistDisabled(true);
+    }
+  };
+
+  const handleDeleteWatchlist = (watchlistId) => {
+    setIsModalOpen(true);
+    setModalContent(
+      <DeleteWatchlistFormModal
+        watchlistId={watchlistId}
+        onClose={() => setIsModalOpen(false)}
+      />
+    );
+  };
+
+  const handleRemoveFunds = () => {
+    setIsModalOpen(true);
+    setModalContent(<RemoveFundsModal onClose={() => setIsModalOpen(false)} />);
   };
 
   const handleAddFunds = () => {
@@ -238,7 +268,10 @@ const PortfolioDetails = () => {
       <div className="portfolio-container">
         <div className="chart-container">
           <div className="detailsgraph">
-            <Line options={options} data={data} />
+            {console.log("chartData inside JSX:", chartData)}
+            <div style={{ height: "400px", width: "600px" }}>
+              {chartData && <Line options={options} data={chartData} />}
+            </div>
           </div>
           {isLoading ? (
             <p>Loading...</p>

c13f843600344d65d2d22c91857e8a4b98f16595
atifrit <adamtifrit@gmail.com>
2023-10-27 13:10:27 -0700

    added company page functionality when not signed in

--- react-app/src/components/Companies/CompanyDetails/index.js
+++ react-app/src/components/Companies/CompanyDetails/index.js
@@ -147,99 +147,155 @@ const CompanyDetails = () => {
     dispatch(getUserWatchlist());
   };
 
-  return (
-    <div className="company-details">
-      <div className="detailsgraph">
-        <Line options={options} data={data} />
-      </div>
+  if(user){
+    return (
+      <div className="company-details">
+        <div className="detailsgraph">
+          <Line options={options} data={data} />
+        </div>
 
-      <div className="company-about">
-        <h2>About</h2>
-        <p>{company.name}</p>
-        <p>{company.ticker}</p>
-        <p>{company.ceo}</p>
-        <p>Headquarters: {company.headquarters}</p>
-        <p>Founded: {company.founded}</p>
-      </div>
+        <div className="company-about">
+          <h2>About</h2>
+          <p>{company.name}</p>
+          <p>{company.ticker}</p>
+          <p>{company.ceo}</p>
+          <p>Headquarters: {company.headquarters}</p>
+          <p>Founded: {company.founded}</p>
+        </div>
 
-      <div className="company-statistics">
-        <h2>Key Statistics</h2>
-        <table>
-          <tbody>
-            <tr>
-              <td>Avg. Volume:</td>
-              <td>{company.avg_volume}</td>
-            </tr>
-            <tr>
-              <td>High:</td>
-              <td>{company.high}</td>
-            </tr>
-            <tr>
-              <td>Low:</td>
-              <td>{company.low}</td>
-            </tr>
-            <tr>
-              <td>Open Price:</td>
-              <td>{company.open_price}</td>
-            </tr>
-            <tr>
-              <td>Volume:</td>
-              <td>{company.volume}</td>
-            </tr>
-            <tr>
-              <td>52 Week High:</td>
-              <td>{company.week_high}</td>
-            </tr>
-            <tr>
-              <td>52 Week Low:</td>
-              <td>{company.week_low}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+        <div className="company-statistics">
+          <h2>Key Statistics</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>Avg. Volume:</td>
+                <td>{company.avg_volume}</td>
+              </tr>
+              <tr>
+                <td>High:</td>
+                <td>{company.high}</td>
+              </tr>
+              <tr>
+                <td>Low:</td>
+                <td>{company.low}</td>
+              </tr>
+              <tr>
+                <td>Open Price:</td>
+                <td>{company.open_price}</td>
+              </tr>
+              <tr>
+                <td>Volume:</td>
+                <td>{company.volume}</td>
+              </tr>
+              <tr>
+                <td>52 Week High:</td>
+                <td>{company.week_high}</td>
+              </tr>
+              <tr>
+                <td>52 Week Low:</td>
+                <td>{company.week_low}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-      <div className="add-to-watchlist">
-        <select
-          onFocus={fetchWatchlists}
-          value={selectedWatchlist}
-          onChange={(e) => setSelectedWatchlist(e.target.value)}
-        >
-          <option value="">Select Watchlist</option>
-          {currentUserWatchlist &&
-            currentUserWatchlist.map((watchlist) => (
-              <option key={watchlist.id} value={watchlist.id}>
-                {watchlist.name}
-              </option>
-            ))}
-        </select>
-        {selectedWatchlist ? (
-          isInWatchlist ? (
-            <button onClick={handleRemoveFromWatchlist}>
-              Remove from Watchlist
-            </button>
+        <div className="add-to-watchlist">
+          <select
+            onFocus={fetchWatchlists}
+            value={selectedWatchlist}
+            onChange={(e) => setSelectedWatchlist(e.target.value)}
+          >
+            <option value="">Select Watchlist</option>
+            {currentUserWatchlist &&
+              currentUserWatchlist.map((watchlist) => (
+                <option key={watchlist.id} value={watchlist.id}>
+                  {watchlist.name}
+                </option>
+              ))}
+          </select>
+          {selectedWatchlist ? (
+            isInWatchlist ? (
+              <button onClick={handleRemoveFromWatchlist}>
+                Remove from Watchlist
+              </button>
+            ) : (
+              <button onClick={handleAddToWatchlist}>Add to Watchlist</button>
+            )
           ) : (
-            <button onClick={handleAddToWatchlist}>Add to Watchlist</button>
-          )
-        ) : (
-          <button onClick={handleAddToWatchlist} disabled>
-            Add to Watchlist
-          </button>
-        )}
+            <button onClick={handleAddToWatchlist} disabled>
+              Add to Watchlist
+            </button>
+          )}
+        </div>
+        <div>
+          <OpenModalButton
+            buttonText="Buy"
+            onItemClick={closeMenu}
+            modalComponent={<BuyFormModal prices={prices} id={user.id} companyId={company.id} portfolio={currentUserPortfolio}/>}
+          />
+          <OpenModalButton
+            buttonText="Sell"
+            onItemClick={closeMenu}
+            modalComponent={<SellFormModal prices={prices} id={user.id} companyId={company.id} user={user} portfolio={currentUserPortfolio} ticker={company.ticker}/>}
+          />
+        </div>
       </div>
-      <div>
-        <OpenModalButton
-          buttonText="Buy"
-          onItemClick={closeMenu}
-          modalComponent={<BuyFormModal prices={prices} id={user.id} companyId={company.id} portfolio={currentUserPortfolio}/>}
-        />
-        <OpenModalButton
-          buttonText="Sell"
-          onItemClick={closeMenu}
-          modalComponent={<SellFormModal prices={prices} id={user.id} companyId={company.id} user={user} portfolio={currentUserPortfolio} ticker={company.ticker}/>}
-        />
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="company-details">
+        <div className="detailsgraph">
+          <Line options={options} data={data} />
+        </div>
+
+        <div className="company-about">
+          <h2>About</h2>
+          <p>{company.name}</p>
+          <p>{company.ticker}</p>
+          <p>{company.ceo}</p>
+          <p>Headquarters: {company.headquarters}</p>
+          <p>Founded: {company.founded}</p>
+        </div>
+
+        <div className="company-statistics">
+          <h2>Key Statistics</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>Avg. Volume:</td>
+                <td>{company.avg_volume}</td>
+              </tr>
+              <tr>
+                <td>High:</td>
+                <td>{company.high}</td>
+              </tr>
+              <tr>
+                <td>Low:</td>
+                <td>{company.low}</td>
+              </tr>
+              <tr>
+                <td>Open Price:</td>
+                <td>{company.open_price}</td>
+              </tr>
+              <tr>
+                <td>Volume:</td>
+                <td>{company.volume}</td>
+              </tr>
+              <tr>
+                <td>52 Week High:</td>
+                <td>{company.week_high}</td>
+              </tr>
+              <tr>
+                <td>52 Week Low:</td>
+                <td>{company.week_low}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        </div>
+    )
+  }
+
 };
 
 export default CompanyDetails;
